4fb23aafacc61942b11795bc95ac5e08236c30c9
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-11 10:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function firstNonRepeatedChar(str) {
+function firstNonRepeatedChar(s) {
  // Write your code here
+	let map = new Map();
+	let str = s.split("");
+	for (let i = 0; i < str.length; i++) {
+		if (map.has(str[i])) {
+			map.set(str[i] , map.get(str[i])+1);
+		}
+		else{
+			map.set(str[i] , 1);
+		}
+	}
+	for (let [key,value] of map.entries()) {
+		if value==1 return key;
+	}
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
