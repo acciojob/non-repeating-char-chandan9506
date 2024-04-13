7d6271a550dc676ac143966ad20194b79748b9fd
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-13 11:01:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function firstNonRepeatedChar(s) {
  // Write your code here
 		let x_or = s[0].charCodeAt();
-	for (let i=1 ; i<s.length ; i++){
+	for (let i=1 ; i < s.length ; i++){
 	  x_or = x_or ^ s[i].charCodeAt();
 	}
 	if (x_or != 0)
-  return String.fromCharCode(x_or);
-  else return "null";
+	  return String.fromCharCode(x_or);
+   else return "null";
 	
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
